42e0cf80d1b8d4cc73b87b385ba805f1c965e2c5
agigibairene <i.agigiba@alustudent.com>
2025-01-02 15:23:53 +0200

    Ecommerce shop

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './Component/Header';
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/Header.jsx
@@ -0,0 +1,49 @@
+import { NavLink } from "react-router-dom";
+import Logo from "../assets/logo.png";
+import { IoMdCart } from "react-icons/io";
+import "../styles/header.css";
+import { useState } from "react";
+
+const routes = [
+    {name: "Shop", route: "/shop"},
+    {name: "Men", route: "/men"},
+    {name: "Women", route: "/women"},
+    {name: "Kids", route: "/kids"},
+]
+
+export default function Header(){
+
+    const [toggleBtn, setToggleBtn] = useState(true)
+
+    function toggleLoginBtn(){
+        setToggleBtn(toggle => !toggle)
+    }
+
+    return(
+        <nav>
+            <div className="logo">
+                <img src={Logo} alt="Logo" />
+                <p>SHOPPER</p>
+            </div>
+
+            <ul className="nav_list">
+                {
+                    routes.map(routeLink =>{
+                        const { name, route} = routeLink;
+                        return(
+                            <li key={route}>
+                                <NavLink className={({isActive})=> isActive ? "active" : ""} to={route}>{name}</NavLink>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+
+            <div className="right">
+                <button onClick={toggleLoginBtn}>{toggleBtn ? "Log Out" : "Login"}</button>
+                <IoMdCart className="icon"/>
+                <div className="nav-cart-count">0</div>
+            </div>
+        </nav>
+    )
+}
